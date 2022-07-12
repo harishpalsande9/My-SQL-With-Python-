aa283fa05207152a0f4020ade315c08bf106663b
harishpalsande9 <harishpalsande9>
2022-07-12 17:30:54 +0530

    create database

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ mydb = conn.connect(host="localhost", user="root", passwd="Root@123")
 print(mydb)
 
 cursor = mydb.cursor()
+# cursor.execute("create database harish")
 cursor.execute("show databases")
 print(cursor.fetchall())
 
-print(cursor.fetchall())
